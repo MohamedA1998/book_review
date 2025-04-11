14e0e999b6c6d5e26b23f955b2a0facb5f77834f
Mo7amed <mohamed.alsayed@cems-it.com>
2025-04-11 17:32:29 +0200

    some update

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', fn() => to_route('book.index') );
 
 Route::resource('book' , BookController::class)->only(['index' , 'show']);
-
+ 
 Route::resource('book.review' , ReviewController::class)
         ->scoped(['reviews' => 'book'])
         ->only(['create' , 'store']);
